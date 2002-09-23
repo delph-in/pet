0b52333e0faf5ba9407f3f7ec455a02d0eed4e76
uc <uc@4200e16c-5112-0410-ac55-d7fb557a720a>
2002-09-23 11:46:48 +0000

    LGPL license added
    git-svn-id: https://pet.opendfki.de/repos/pet/main@10 4200e16c-5112-0410-ac55-d7fb557a720a

--- cheap/inputchart.h
+++ cheap/inputchart.h
@@ -1,6 +1,21 @@
 /* PET
  * Platform for Experimentation with efficient HPSG processing Techniques
- * 2001 Bernd Kiefer kiefer@dfki.de
+ * (C) 1999 - 2002 Ulrich Callmeier uc@coli.uni-sb.de
+ * (C) 2001 Bernd Kiefer kiefer@dfki.de
+ *
+ *   This program is free software; you can redistribute it and/or
+ *   modify it under the terms of the GNU Lesser General Public
+ *   License as published by the Free Software Foundation; either
+ *   version 2.1 of the License, or (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *   Lesser General Public License for more details.
+ *
+ *   You should have received a copy of the GNU Lesser General Public
+ *   License along with this library; if not, write to the Free Software
+ *   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
 
 // input_chart implements a (quite) generic input token interface
--- cheap/yy.h
+++ cheap/yy.h
@@ -1,10 +1,21 @@
-//
-//      The entire contents of this file, in printed or electronic form, are
-//      (c) Copyright YY Technologies, Mountain View, CA 1991,1992,1993,1994,
-//                                                       1995,1996,1997,1998,
-//                                                       1999,2000,2001
-//      Unpublished work -- all rights reserved -- proprietary, trade secret
-//
+/* PET
+ * Platform for Experimentation with efficient HPSG processing Techniques
+ * (C) 1999 - 2002 Ulrich Callmeier uc@coli.uni-sb.de
+ *
+ *   This program is free software; you can redistribute it and/or
+ *   modify it under the terms of the GNU Lesser General Public
+ *   License as published by the Free Software Foundation; either
+ *   version 2.1 of the License, or (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *   Lesser General Public License for more details.
+ *
+ *   You should have received a copy of the GNU Lesser General Public
+ *   License along with this library; if not, write to the Free Software
+ *   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
 
 // YY input format and server mode (oe, uc)
 
