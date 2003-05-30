63e2fb7e9022c54adb24175f9393cc543a92db8b
uc <uc@4200e16c-5112-0410-ac55-d7fb557a720a>
2003-05-30 03:19:32 +0000

    Version number tracking.
    git-svn-id: https://pet.opendfki.de/repos/pet/main@95 4200e16c-5112-0410-ac55-d7fb557a720a

---
+++ cheap/version.h
@@ -0,0 +1,28 @@
+/* PET
+ * Platform for Experimentation with efficient HPSG processing Techniques
+ * (C) 1999 - 2002 Ulrich Callmeier uc@coli.uni-sb.de
+ *
+ *   This program is free software; you can redistribute it and/or
+ *   modify it under the terms of the GNU Lesser General Public
+ *   License as published by the Free Software Foundation; either
+ *   version 2.1 of the License, or (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *   Lesser General Public License for more details.
+ *
+ *   You should have received a copy of the GNU Lesser General Public
+ *   License along with this library; if not, write to the Free Software
+ *   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+/* version information */
+
+#ifndef _VERSION_H_
+#define _VERSION_H_
+
+#define VERSION_DATETIME "$DateTime"
+#define VERSION_CHANGE "$Change"
+
+#endif
