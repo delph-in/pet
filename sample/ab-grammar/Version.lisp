caf53f5a601c93735e095c491dba780d83f4d8a0
uc <uc@4200e16c-5112-0410-ac55-d7fb557a720a>
2003-06-30 15:04:25 +0000

    Latest a-b version from Frederik.
    git-svn-id: https://pet.opendfki.de/repos/pet/main@175 4200e16c-5112-0410-ac55-d7fb557a720a

--- sample/ab-grammar/Version.lisp
+++ sample/ab-grammar/Version.lisp
@@ -1,5 +1,8 @@
 (in-package :common-lisp-user)
 
-;;; set grammar version for both LKB and PET
+;;;
+;;; set grammar version for both LKB and PAGE; this should substitute for all
+;;; assignments in either `lkb/script' or `english.tdl'.  (2-feb-99  -  oe@eo)
+;;;
 
 (defparameter *grammar-version* "AB (Mar-03)")
