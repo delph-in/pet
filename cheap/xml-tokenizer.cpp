802d0c4155fa0fc9aefb8bac9cab630d23c1f5f5
ben_waldron <ben_waldron@4200e16c-5112-0410-ac55-d7fb557a720a>
2006-07-15 23:20:41 +0000

    add xml-tokenizer.cpp
    git-svn-id: https://pet.opendfki.de/repos/pet/main@254 4200e16c-5112-0410-ac55-d7fb557a720a

---
+++ cheap/xml-tokenizer.cpp
@@ -0,0 +1,63 @@
+/* PET
+ * Platform for Experimentation with efficient HPSG processing Techniques
+ * (C) 1999 - 2002 Ulrich Callmeier uc@coli.uni-sb.de
+ *   2004 Bernd Kiefer bk@dfki.de
+ *
+ *   This program is free software; you can redistribute it and/or
+ *   modify it under the terms of the GNU Lesser General Public
+ *   License as published by the Free Software Foundation; either
+ *   version 2.1 of the License, or (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *   Lesser General Public License for more details.
+ *
+ *   You should have received a copy of the GNU Lesser General Public
+ *   License along with this library; if not, write to the Free Software
+ *   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+#include "xml-tokenizer.h"
+#include "cheap.h"
+
+/** Produce a set of tokens from the given XML input. */
+void tXMLTokenizer::tokenize(string input, inp_list &result) {
+
+  if (input.compare(0, 2, "<?") == 0)
+    tokenize_from_stream(input, result);
+  else
+    tokenize_from_file(input, result);
+}
+
+/** Produce a set of tInputItem tokens from the given XML input on stdin. */
+void tXMLTokenizer::tokenize_from_stream(string input, inp_list &result) {
+  string buffer = input;
+  if(verbosity > 4)
+    {
+      //cerr << "received from :pic preprocessor:" << endl << buffer << endl << endl;
+      fprintf(ferr, "[processing PIC XML input]\n");
+    };
+  
+  PICHandler picreader(true, _translate_iso_chars);
+  MemBufInputSource xmlinput((const XMLByte *) buffer.c_str()
+			     , buffer.length(), "STDIN");
+  if (parse_file(xmlinput, &picreader)
+      && (! picreader.error())) {
+    result = picreader.items();
+    //    result.splice(result.begin(), picreader.items());
+  }
+}
+
+/** Produce a set of tokens from the given XML file. */
+void tXMLTokenizer::tokenize_from_file(string filename, inp_list &result) {
+  PICHandler picreader(true, _translate_iso_chars);
+  XMLCh * XMLFilename = XMLString::transcode(filename.c_str());
+  LocalFileInputSource inputfile(XMLFilename);
+  if (parse_file(inputfile, &picreader)
+      && (! picreader.error())) {
+    result.splice(result.begin(), picreader.items());
+  }
+  XMLString::release(&XMLFilename);
+}
+
