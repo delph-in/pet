0212530d6e2f40546dbb22382ac3f1f725a52c82
tomasz_tylenda <tomasz_tylenda@4200e16c-5112-0410-ac55-d7fb557a720a>
2007-01-16 18:17:28 +0000

    Some code moved from Configuration.h to ConfigurationInternal.h
    git-svn-id: https://pet.opendfki.de/repos/pet/branches/configuration-branch@280 4200e16c-5112-0410-ac55-d7fb557a720a

--- common/Configuration.h
+++ common/Configuration.h
@@ -8,19 +8,7 @@
 #include <typeinfo>
 #include <functional>
 
-#if HAVE_LIBLOG4CXX
-#  include <log4cxx/logger.h>
-#  include <log4cxx/basicconfigurator.h>
-#  include <sstream>
-#else
-#  ifndef LOG4CXX_DEBUG // despite lack of log4cxx if can be defined by user
-#    define LOG4CXX_DEBUG(logger, msg) ;
-#    define LOG4CXX_INFO(logger, msg) ;
-#    define LOG4CXX_WARN(logger, msg) ;
-#    define LOG4CXX_ERROR(logger, msg) ;
-#    define LOG4CXX_FATAL(logger, msg) ;
-#  endif // LOG4CXX_DEBUG
-#endif // HAVE_LIBLOG4CXX
+#include "ConfigurationInternal.h"
 
 /** @file Configuration.h
  *   @brief System for managing various configuration options in a program.
@@ -43,126 +31,6 @@ class WrongEntryNameException : public ConfigurationException {};
 /** @brief Entry with given name already exists in configuration. */
 class EntryAlreadyExistsException : public ConfigurationException {};
 
-
-/** @brief Root class for managing one option. */
-class IOption {
-public:
-  IOption() : greatestPriority_(0) {}
-
-  virtual ~IOption() {}
-
-protected:
-  /**
-   * Check if the given priority is greatest or equal to the highest
-   * priority that has already been used. If yes than record it as
-   * a new highest priority. See #Option<T>::set for details.
-   * @param priority priority to check
-   * @return true if the given priority is greatest or equal to the highest
-   *              priority that has already been used
-   */
-  inline bool checkAndSetPriority(int priority) {
-    if (priority >= greatestPriority_) {
-      greatestPriority_ = priority;
-      return true;
-    }
-    return false;
-  }
-
-private:   
-  int greatestPriority_; //greatest value used in set()
-};
-
-/**
- * @brief Abstract class for managing one option of given type.
- */
-template<class T> class Option : public IOption {
-public:
-  virtual ~Option() {}
-
-  /** Returns value of the option. */
-  virtual T& get() = 0;
-
-  /**
-   * Set value of the option.
-   * @param value New value.
-   * @param priority If priority is equal or grater than the one used in 
-   *       previous calls, then assignment takes place. Otherwise the call is
-   *       ignored. Eg. you can use priority = 10 for configuration file, and
-   *       20 for command line options, then command line always overrides
-   *       configuration file, even if configuration file is processed later.
-   */
-  virtual void set(const T& value, int priority = 1) = 0;
-};
-
-/**
- * @brief  Concrete classes for managing options.
- * 
- * Object of this class stores the value of an option internally, as a field.
- */
-template<class T>
-class HandledOption : public Option<T> {
-public:
-  virtual ~HandledOption() {}
-
-  /** Returns value of the option. */
-  virtual T& get() { return value_; }
-
-  /**
-   * Set value of the option.
-   * @param value New value.
-   * @param priority See parent class for details.
-   * @see Option::set
-   */
-  virtual void set(const T& value, int priority = 1) {
-    if (IOption::checkAndSetPriority(priority))
-      value_ = value;
-  }
-  
-private:
-  //we always acccess data using value_, but it can point to a object whose
-  //memory is handled by the \c Option
-  T value_;
-};
-
-/**
- * @brief Concrete classes for managing options.
- *
- * Object of this class stores a pointer to location of option's variable.
- */
-template<class T>
-class ReferenceOption : public Option<T> {
-public:
-  /**
-   * If object is created with this constructor, then it uses external (via
-   * reference) variable for value of the option.
-   * @param valueRef Reference to variable which stores value of the option.
-   */
-  ReferenceOption(T* valueRef) : value_(valueRef) {}
-
-  virtual ~ReferenceOption() {}
-
-  /** Returns value of the option. */
-  virtual T& get() { return *value_; }
-
-  /**
-   * Set value of the option.
-   * @param value New value.
-   * @param priority See parent class for details.
-   * @see Option::set
-   */
-  virtual void set(const T& value, int priority = 1) {
-    if (IOption::checkAndSetPriority(priority))
-      *value_ = value;
-  }
-
-private:
-  ReferenceOption() {}
-
-  //we always acccess data using value_, but it can point to a object whose
-  //memory is handled by the \c Option
-  T* value_;
-};
-
 /**
  * @brief Helper class for managing options with callback functions.
  *
@@ -184,44 +52,6 @@ public:
    * @return Current value of the option.
    */
   virtual T& get() = 0;
-};
-
-/**
- * @brief Concrete classes for managing callback options of given type.
- *
- * An user-provided callback is responsible for storing a value of an option.
- */
-template<class T>
-class CallbackOption : public Option<T> {
-public:
-  /**
-   * If object is created with this constructor, sets the value via the
-   * callback
-   * @param callback Function-object which is called every time value of option
-   *                 is changed
-   */
-  CallbackOption(Callback<T>* callback) : callback_(*callback) {}
-
-  virtual ~CallbackOption() {}
-
-  /** Returns value of the option. */
-  virtual T& get() { 
-    return callback_.get();
-  }
-
-  /**
-   * Set value of the option.
-   * @param value New value.
-   * @param priority See parent class for details.
-   * @see Option::set
-   */
-  virtual void set(const T &value, int priority = 1) {
-    if (IOption::checkAndSetPriority(priority))
-      callback_.set(value);
-  }
-
-private:
-  Callback<T>& callback_;
 };
 
 /**
