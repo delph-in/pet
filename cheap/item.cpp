e6ebb4c54304ffed42af3a193990b637e41750af
yi_zhang <yi_zhang@4200e16c-5112-0410-ac55-d7fb557a720a>
2007-03-07 10:42:23 +0000

    improved instantiation failure propagation in selective unpacking
    git-svn-id: https://pet.opendfki.de/repos/pet/main@305 4200e16c-5112-0410-ac55-d7fb557a720a

--- cheap/item.cpp
+++ cheap/item.cpp
@@ -1320,7 +1320,8 @@ tPhrasalItem::hypothesize_edge(list<tItem*> path, unsigned int i)
       } else // every thing fine, create the new hypothesis
 	new_hypothesis(hypo->decomposition, dtrs, indices);
     }
-    _hypotheses_path[path].push_back(hypo);
+    if (!hypo->inst_failed)
+      _hypotheses_path[path].push_back(hypo);
   }
   if (i < _hypotheses_path[path].size()){
     if (_hypo_agendas[path].size() == 0)
