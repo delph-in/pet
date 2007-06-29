4707f6fa638582b908dde5a25f0547161e4ef670
pead01 <pead01@4200e16c-5112-0410-ac55-d7fb557a720a>
2007-06-29 16:38:50 +0000

    Undoing change committed in r345. (I've got no idea what happened there and why.)
    git-svn-id: https://pet.opendfki.de/repos/pet/branches/input-chart-rules@346 4200e16c-5112-0410-ac55-d7fb557a720a

--- main/sample/ab-grammar/Version.lisp
+++
@@ -1,8 +0,0 @@
-(in-package :common-lisp-user)
-
-;;;
-;;; set grammar version for both LKB and PAGE; this should substitute for all
-;;; assignments in either `lkb/script' or `english.tdl'.  (2-feb-99  -  oe@eo)
-;;;
-
-(defparameter *grammar-version* "AB (Mar-03)")
