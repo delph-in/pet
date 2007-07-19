a7668ab1d6e79658932f4767eaf04644a0bdec25
pead01 <pead01@4200e16c-5112-0410-ac55-d7fb557a720a>
2007-07-19 15:28:23 +0000

    changes to the build system   - made doxygen documentation buildable in any build path   - respect user-provided variables such as CXXFLAGS in ./configure   - don't try to override predefined -g -O flags with AM_CXXFLAGS
    git-svn-id: https://pet.opendfki.de/repos/pet/branches/input-chart-rules@347 4200e16c-5112-0410-ac55-d7fb557a720a

--- cheap/test/tester.cpp
+++ cheap/test/tester.cpp
@@ -12,18 +12,40 @@
 #include <cppunit/BriefTestProgressListener.h>
 #include <cppunit/CompilerOutputter.h>
 
+#include <string>
+
+#include "grammar.h"
+#include "settings.h"
+
+// required global settings from cheap.cpp 
+char * version_string = VERSION ;
+FILE* ferr = stderr;
+FILE* fstatus = stderr;
+FILE* flog = NULL;
+bool XMLServices = false;
+tGrammar *Grammar;
+settings *cheap_settings;
+
 int
 main(int argc, char **argv)
 {
+  // initialize grammar:
+  char* grmstr = "/home/peter/Eclipse/pet-icr/sample/tokmap-grammar/grammar.grm";
+  std::string base = raw_name(grmstr);
+  cheap_settings = new settings(base.c_str(), grmstr, "reading");
+  Grammar = new tGrammar(grmstr);
+  
+  // create and setup unit testing objects:
   CppUnit::TestResultCollector result;
   CppUnit::CompilerOutputter outputter(&result, std::cerr);
   CppUnit::BriefTestProgressListener progress;
   CppUnit::TestResult controller;
   CppUnit::TestRunner runner;
-  
   controller.addListener(&result);
   controller.addListener(&progress);      
   runner.addTest(CppUnit::TestFactoryRegistry::getRegistry().makeTest());
+  
+  // run tests:
   runner.run(controller);
   outputter.write();    
   
